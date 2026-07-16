0a86abf6b85c8574b0a189318dc8ab5905dec5a7
Aanid A Das <aanidadas@gmail.com>
2026-07-16 20:21:29 +0530

    0207-COURSE-SCHEDULE Stats: Time: 327 ms (5.02%), Space: 187.5 MB (5.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -21,12 +21,46 @@ public:
         pvis[src]=0;
         return false;
     }
-    
+    bool BFS(int n,vector<vector<int>>& p)
+    {
+        vector<int> indeg(n,0);
+        queue<int> q;
+        for(int i = 0;i<n;i++)
+        {
+            for(auto it:p[i]) indeg[it]++;
+        }
+        int count =0;
+        for(int i =0;i<n;i++)
+        {
+           if(indeg[i]==0) q.push(i);
+           
+        }
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            count++;
+            for(auto it:p[node])
+            {
+                indeg[it]--;
+                if(indeg[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        if(count ==n)
+        {
+            return false;
+        }
+        return true;
+    }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<vector<int>> p(n);
         for(auto it : pre)
         {
-            p[it[0]].emplace_back(it[1]);
+            p[it[1]].emplace_back(it[0]);
         }
         vector<int> vis(n,0);
         vector<int> pvis(n,0);
@@ -34,7 +68,8 @@ public:
         {
             if(!vis[i])
             {
-                if(DFS(i,p,vis,pvis)) return false;
+                //if(DFS(i,p,vis,pvis)) return false;
+                if(BFS(n,p)) return false;
             }
         }
         return true;

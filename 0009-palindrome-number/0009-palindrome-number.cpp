5077520140ae5f371d18f1336ca49d717bb89975
Aanid A Das <aanidadas@gmail.com>
2026-07-22 16:22:57 +0530

    0009-PALINDROME-NUMBER Stats: Time: 0 ms (100%), Space: 11 MB (7.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string s = to_string(x);
-        string ass = s;
-        reverse(ass.begin(),ass.end());
-        if(ass==s) return true;
-        else return false;
+        int l =0, r = s.size()-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
         
     }
 };

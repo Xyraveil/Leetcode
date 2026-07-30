059e8780c9f849d66999ba97c2189cf4dce64983
Aanid A Das <aanidadas@gmail.com>
2026-07-30 18:42:22 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 2 ms (31.11%), Space: 97.4 MB (25.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int mx = INT_MIN;
-        vector<int> mxnext(n,0);
-        mxnext[n-1]= prices[n-1];
-        for(int i =n-2;i>=0;i--)
+        int mxpr = 0;
+        int sellday =1;
+        int buy = prices[0];
+        for(int i =0;i<n;i++)
         {
-            mxnext[i]=max(prices[i],mxnext[i+1]);
+            if(prices[i]<buy)
+            {
+                buy = prices[i];
+            }
+            else if(prices[i]-buy>mxpr)
+            {
+                mxpr = prices[i]-buy;
+
+            }
         }
-        for(int i =0;i<n-1;i++)
-        {
-            mx = max(mxnext[i+1]-prices[i],mx);
-        }
-        if(mx>0)return mx;
-        else return 0;
+        return mxpr;
     }
 };

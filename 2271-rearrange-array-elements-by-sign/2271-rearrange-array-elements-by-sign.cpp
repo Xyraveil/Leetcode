1b3ad400cbc641b5088b2f8feac003540c8eafd5
Aanid A Das <aanidadas@gmail.com>
2026-07-30 19:10:24 +0530

    2271-REARRANGE-ARRAY-ELEMENTS-BY-SIGN Stats: Time: 7 ms (55.49%), Space: 128.2 MB (95.7%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -3,16 +3,21 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> negga,pos;
+        int posind =0;
+        int negind = 1;
+        vector<int> Ans(n); 
         for(int i =0;i<n;i++)
         {
-            if(nums[i]<0) negga.emplace_back(nums[i]);
-            else pos.emplace_back(nums[i]);
-        }
-        vector<int> Ans; 
-        for(int i = 0;i<n/2;i++)
-        {
-            Ans.emplace_back(pos[i]);
-            Ans.emplace_back(negga[i]);
+            if(nums[i]<0) 
+            {
+                Ans[negind] = nums[i];
+                negind+=2;
+            }
+            else
+            {
+                Ans[posind] = nums[i];
+                posind+=2;
+            }
         }
         return Ans;
     }

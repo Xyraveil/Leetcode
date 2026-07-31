a6c4952d4540f77b30abcce87d9f85b73e75f2de
Aanid A Das <aanidadas@gmail.com>
2026-07-31 13:40:09 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 201 ms (5.04%), Space: 91.3 MB (8.17%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,36 +2,58 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        vector<int> vis(n,0);
-        int mxlen = INT_MIN;
-        map<int,stack<int>> M;
-        for(int i = 0;i<n;i++)
-        {
-            M[nums[i]].push(i);
-        }
-        for(int i = 0;i<n;i++)
-        {
-            if(vis[i]==0)
-            {
-                int high = nums[i]+1;
-                int low = nums[i]-1;
-                while(!M[high].empty())
-                {
-                    vis[M[high].top()] =1;
-                    M[high].pop();
-                    high++;
+        // vector<int> vis(n,0);
+        // int mxlen = INT_MIN;
+        // map<int,stack<int>> M;
+        // for(int i = 0;i<n;i++)
+        // {
+        //     M[nums[i]].push(i);
+        // }
+        // for(int i = 0;i<n;i++)
+        // {
+        //     if(vis[i]==0)
+        //     {
+        //         int high = nums[i]+1;
+        //         int low = nums[i]-1;
+        //         while(!M[high].empty())
+        //         {
+        //             vis[M[high].top()] =1;
+        //             M[high].pop();
+        //             high++;
                     
-                }
-                while(!M[low].empty())
+        //         }
+        //         while(!M[low].empty())
+        //         {
+        //             vis[M[low].top()] =1;
+        //             M[low].pop();
+        //             low--;
+        //         }
+        //         mxlen = max(mxlen, high - low -1 );
+        //     }
+        // }
+        // if(mxlen<0) return 0;
+        // return mxlen;
+        set<int> S;
+        for(int i =0;i<n;i++)
+        {
+            S.insert(nums[i]);
+        }
+        int mxlen =0;
+        
+        for(int i: S)
+        {
+            if(S.find(i-1)==S.end()) 
+            {
+                int count =1;
+                int j = i;
+                while(S.find(j+1)!=S.end())
                 {
-                    vis[M[low].top()] =1;
-                    M[low].pop();
-                    low--;
+                    count++;
+                    j++;
                 }
-                mxlen = max(mxlen, high - low -1 );
+                mxlen = max(mxlen, count);
             }
         }
-        if(mxlen<0) return 0;
         return mxlen;
     }
 };
